daa21f796d6aa52ef22eaaef5f82d1a9818430dc
Falon Darville <falondarville@gmail.com>
2018-09-25 10:14:31 -0700

    axios

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
-var express = require('express');
-var nodemon = require('nodemon');
+const express = require('express');
+const nodemon = require('nodemon');
+const axios = require('axios');
+require('dotenv').config();
 
-var app = express();
-var PORT = process.env.PORT || 3000;
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+username = process.env.USERNAME;
+password = process.env.PASSWORD;
+
+axios.get('http://qa-api-carrierx.int/core/v2/endpoints?login=' + username + '&password=' + password)
+	.then(function(response){
+		console.log(response)
+	})
+	.catch(function(error){
+		console.log(error)
+	})
 
 app.listen(PORT, function(){
     console.log(`The server is up and running on port: ${PORT} .`);
